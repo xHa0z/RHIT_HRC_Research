32fb771ef79c5f72d94234afc6d1a1fbcd19d863
frolo10 <doughezj@rose-hulman.edu>
2016-09-24 12:59:48 -0400

    added basic C++ program to work with Cyton Epsilon

---
+++ CytonEpsilon/CytonEpsilon/stdafx.h
@@ -0,0 +1,30 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+
+//Cyton methods to include
+#include <control/ecEndEffectorSet.h>
+#include <controlCore/ecFrameEndEffector.h>
+#include <control/ecManipEndEffectorPlace.h>
+#include <foundCommon/ecCoordSysXForm.h>
+#include <foundCore/ecApplication.h>
+#include <foundCore/ecConstants.h>
+#include <foundCore/ecMacros.h>
+#include <foundCore/ecTypes.h>
+#include <manipulation/ecManipulationActionManager.h>
+#include <manipulationDirector/ecManipulationScript.h>
+#include <remoteCommand/ecRemoteCommand.h>
+
+
+
+// TODO: reference additional headers your program requires here
