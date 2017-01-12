04a17fe121759f45698f6e77897e04207f44a654
bdgish <gishbd@rose-hulman.edu>
2017-01-12 17:10:08 -0500

    Add files via upload

--- Main-Program/circular_array.py
+++ Main-Program/circular_array.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 class CircularArray:
 
 
@@ -7,9 +7,15 @@ class CircularArray:
 
         self.array = [np.NaN] * size
         self.index = 0
+        self.counter = 1
+
     def add_value(self, value):
+
+
         self.array[self.index] = value
+
         self.index = (self.index + 1) % (len(self.array))
+        self.counter += 1
     def averager_function(self):
         count = 0
         sum = 0
@@ -29,4 +35,25 @@ class CircularArray:
             return sum_avg
 
 
+    def standard_deviation(self, size):
 
+        std = .001
+        if self.counter < size + 1:
+
+
+            for k in range(0, len(self.array)):
+                print
+                print self.counter
+                print
+                std += ((self.array[k] - self.averager_function()) ** 2)
+                return (math.sqrt(std) / self.counter)
+
+
+        elif self.counter >= size + 1:
+            ff = .001
+            for k in range(0, len(self.array)):
+                print
+                print self.counter
+                print
+                ff += ((self.array[k] - self.averager_function()) ** 2)
+                return (math.sqrt(ff) / len((self.array)))
