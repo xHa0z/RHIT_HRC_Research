b5bddb17867332c12fe34014b3fcafd922f31333
LinuxDevon <dachicagobears7@gmail.com>
2017-01-17 17:00:01 -0500

    updated

--- Main-Program/GUI_Main.py
+++ Main-Program/GUI_Main.py
@@ -68,6 +68,7 @@ from MainFunctions import getMatrixFromFile, \
                           resetTextFile
 import MainFunctions
 
+from Custom_Timer import TimerReset
 
 import modular_prob_dist_sliding_window as lpp
 from modular_prob_dist_sliding_window import Leap_Matrix
@@ -102,57 +103,8 @@ with open('out_file.txt', 'w') as f:
 # with open('Box_Selected.txt', 'w') as f:
 #     f.write('')  
 
-def TimerReset(*args, **kwargs):
-    """ Global function for Timer """
-    return _TimerReset(*args, **kwargs)
 
 
-class _TimerReset(threading.Thread):
-    """Call a function after a specified number of seconds:
-
-    t = TimerReset(30.0, f, args=[], kwargs={})
-    t.start()
-    t.cancel() # stop the timer's action if it's still waiting
-    """
-
-    def __init__(self, interval, function, args=[], kwargs={}):
-        Thread.__init__(self)
-        self.interval = interval
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.finished = Event()
-        self.resetted = True
-
-    def cancel(self):
-        """Stop the timer if it hasn't finished yet"""
-        self.finished.set()
-
-    def run(self):
-        print ("Time: %s - timer running..." % time.asctime())
-
-        while self.resetted:
-            print ("Time: %s - timer waiting for timeout in %.2f..." % (time.asctime(), self.interval))
-            self.resetted = False
-            self.finished.wait(self.interval)
-
-        if not self.finished.isSet():
-            self.function(*self.args, **self.kwargs)
-        self.finished.set()
-        print ("Time: %s - timer finished!" % time.asctime())
-
-    def reset(self, interval=None):
-        """ Reset the timer """
-
-        if interval:
-            print ("Time: %s - timer resetting to %.2f..." % (time.asctime(), interval))
-            self.interval = interval
-        else:
-            print( "Time: %s - timer resetting..." % time.asctime())
-
-        self.resetted = True
-        self.finished.set()
-        self.finished.clear()
         
 reset_array = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])    
 np.savetxt('Leap_Matrix.txt', reset_array, fmt='%1d') 
